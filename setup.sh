00f049f6306a24cf3b8728b51e2bf1d381282b0d
nuno120 <nunoscholten@gmail.com>
2024-12-30 21:19:40 +0100

    execvute main in setup.sh

--- setup.sh
+++ setup.sh
@@ -89,11 +89,13 @@ function logTimestamp() {
 }
 
 function setupTimezone() {
-    echo -ne "Enter the timezone for the server (Default is 'Asia/Singapore'):\n" >&3
+    echo -ne "Enter the timezone for the server (Default is 'Europe/Amsterdam'):\n" >&3
     read -r timezone
     if [ -z "${timezone}" ]; then
-        timezone="Asia/Singapore"
+        timezone="Europe/Amsterdam"
     fi
     setTimezone "${timezone}"
     echo "Timezone is set to $(cat /etc/timezone)" >&3
 }
+
+main
